bd571a2f1a12b727895eb67cf0d99ae7b828466a
rushiiiii <91418267+rushiiiii@users.noreply.github.com>
2024-05-08 14:08:13 +0530

    Delete Data.m

--- Data.m
+++
@@ -1,9 +0,0 @@
-function [P,S,F,WC] = Data
-% number of high, middle and low class population
-P=[200000 270000 270000 180000]';
-% salary of each type of worker
-S=[300000 200000 135000 90000 72000 45000]';
-% avg extra requirements from each type of occupant at each house
-F=[1000 400 300 150]';
-% working capacity
-WC=[1350 1080 900 2700 1800 900]';
